121c444e91367ab464f73e2d18d37b82ce43e314
Reza Najafi <mrnj92@gmail.com>
2024-04-15 11:20:47 +0200

    Completes Birdy to include actual client logic

--- birdy/src/main/java/com/flydroid/birdy/Birdy.kt
+++ birdy/src/main/java/com/flydroid/birdy/Birdy.kt
@@ -1,4 +1,48 @@
 package com.flydroid.birdy
 
+import android.content.Context
+import com.flydroid.birdy.domain.ObserveParams
+import com.flydroid.birdy.domain.OneshotRequest
+
 object Birdy {
+    private var birdy: BirdyClient? = null
+
+    fun init(
+        context: Context,
+        debugMode: Boolean,
+        apiKey: String,
+        debugLogger: DebugLogger = DebugLoggerDefault(debugMode)
+    ) {
+        birdy = BirdyGooglePlayServices(
+            appContext = context.applicationContext,
+            debugMode = debugMode,
+            apiKey = apiKey,
+            debugLogger = debugLogger
+        )
+    }
+
+    fun startLocationUpdates(request: ObserveParams) {
+        if (birdy == null) {
+            println("Birdy is not initialized")
+            return
+        }
+        birdy?.startLocationUpdates(request)
+    }
+
+    fun stopLocationUpdates() {
+        if (birdy == null) {
+            println("Birdy is not initialized")
+            return
+        }
+        birdy?.stopLocationUpdates()
+    }
+
+    fun requestSingleUpdate(request: OneshotRequest) {
+        if (birdy == null) {
+            println("Birdy is not initialized")
+            return
+        }
+        birdy?.requestSingleUpdate(request)
+    }
+
 }
