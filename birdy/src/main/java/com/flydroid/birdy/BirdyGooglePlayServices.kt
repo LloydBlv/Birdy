8448f487dcc43031dc51dfc09a046ea3cbc48a4a
Reza Najafi <mrnj92@gmail.com>
2024-04-15 11:08:58 +0200

    Adds Birdy's Google play services implementation

---
+++ birdy/src/main/java/com/flydroid/birdy/BirdyGooglePlayServices.kt
@@ -0,0 +1,71 @@
+package com.flydroid.birdy
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.util.Log
+import androidx.annotation.VisibleForTesting
+import com.flydroid.birdy.domain.ObserveParams
+import com.flydroid.birdy.domain.OneshotRequest
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+internal class BirdyGooglePlayServices(
+    private val appContext: Context,
+    private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO,
+    debugMode: Boolean = false,
+    private val debugLogger: DebugLogger = DebugLoggerDefault(debugMode),
+    private val locationProvider: LocationProvider = LocationProviderDefault(
+        appContext,
+        debugLogger
+    ),
+    apiKey: String,
+    private val locationSyncer: LocationSyncer = LocationSyncerDefault(appContext, debugLogger, apiKey = apiKey)
+) : BirdyClient {
+
+    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    val handler = CoroutineExceptionHandler { _, exception ->
+        debugLogger.logException(exception)
+    }
+
+    init {
+        locationProvider.setLocationReceivedListener { latitude, longitude ->
+            CoroutineScope(ioDispatcher + handler).launch {
+                locationSyncer.syncLocation(latitude, longitude)
+            }
+        }
+    }
+
+    override fun startLocationUpdates(request: ObserveParams) {
+        when {
+            !hasLocationPermission() -> debugLogger.log("Location permission is not granted", Log.ERROR)
+            locationProvider.isObserving -> debugLogger.log("Location updates already started")
+            else -> locationProvider.requestUpdates(request)
+        }
+    }
+
+    override fun stopLocationUpdates() {
+        debugLogger.log("stopLocationUpdates")
+        locationProvider.removeUpdates()
+
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun requestSingleUpdate(
+        request: OneshotRequest
+    ) {
+        debugLogger.log("requestSingleUpdate")
+        when {
+            !hasLocationPermission() -> debugLogger.log("Location permission is not granted", Log.ERROR)
+            request is OneshotRequest.LastKnownLocation -> locationProvider.requestLastLocation(request)
+            request is OneshotRequest.FreshLocation -> locationProvider.requestCurrentLocation(request)
+        }
+    }
+
+    private fun hasLocationPermission(): Boolean {
+        return appContext.hasFineLocationPermission()
+                || appContext.hasCoarseLocationPermission()
+    }
+}
