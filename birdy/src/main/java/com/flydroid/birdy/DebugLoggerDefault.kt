8059ee845e71338941e8ba93e404d8b5db583616
Reza Najafi <mrnj92@gmail.com>
2024-04-15 11:02:48 +0200

    Adds DebugLoggerDefault

---
+++ birdy/src/main/java/com/flydroid/birdy/DebugLoggerDefault.kt
@@ -0,0 +1,21 @@
+package com.flydroid.birdy
+
+import android.util.Log
+
+private const val TAG = "Birdy"
+class DebugLoggerDefault(private val debugEnabled: Boolean): DebugLogger {
+    override fun log(message: String, level: Int) {
+        if(!debugEnabled) return
+        when(level) {
+            Log.DEBUG -> Log.d(TAG, message)
+            Log.INFO -> Log.i(TAG, message)
+            Log.WARN -> Log.w(TAG, message)
+            Log.ERROR -> Log.e(TAG, message)
+        }
+    }
+    override fun logException(exception: Throwable) {
+        if(!debugEnabled) return
+        Log.e(TAG, "Exception: ${exception.message}", exception)
+    }
+
+}
